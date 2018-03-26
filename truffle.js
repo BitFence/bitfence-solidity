daca2e0e0e6f73e206929b8e53b3419b353e1200
BitFence <contact@bitfence.io>
2018-03-25 23:20:03 -0400

    Updated configuration

--- truffle.js
+++ truffle.js
@@ -1,4 +1,13 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+solc: { optimizer: { enabled: false, runs: 200 } },
+
+  networks: {
+   development: {
+     host: "localhost",
+     port: 8545,
+     network_id: "*" // Match any network id
+   }
+ }
 };
